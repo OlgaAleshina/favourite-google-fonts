3c80dbfa04824d3b8ec869bd27071e95bdb6dfa1
Olga <ovg43@yandex.ru>
2020-01-02 11:37:44 +0300

    delete import bootstrap

--- src/containers/FilterableFontTable.jsx
+++ src/containers/FilterableFontTable.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import FontScripts from "./FontScripts";
 import FilterNavbar from "./FilterNavbar";
 import FontTable from "./FontTable";
